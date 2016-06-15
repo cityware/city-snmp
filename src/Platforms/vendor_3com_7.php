8282364b9d98a29cee8938ea9aab405105f14d57
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-06-15 14:55:56 -0300

    Update

--- src/Platforms/vendor_3com_7.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (strstr($sysDescr, '3Com Switch ') or strstr($sysDescr, '3Com SuperStack') or strstr($sysDescr, '3Com Baseline')) {
-    $this->setVendor('3Com Corporation');
-    $this->setModel('Generic');
-    $this->setOs('3Com');
-}
