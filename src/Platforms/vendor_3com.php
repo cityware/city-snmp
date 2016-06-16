a3123bcd29b84e73836991db957bd6ddcda54e1b
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-06-16 09:44:11 -0300

    Update

--- src/Platforms/vendor_3com.php
+++ src/Platforms/vendor_3com.php
@@ -1,6 +1,6 @@
 <?php
 
-if (strstr($sysDescr, '3Com Switch ') or strstr($sysDescr, '3Com SuperStack') or strstr($sysDescr, '3Com Baseline')) {
+if (preg_match('/^3Com Switch/', $sysDescr) or preg_match('/^3Com SuperStack/', $sysDescr) or preg_match('/^3Com Baseline/', $sysDescr)) {
     $this->setVendor('3Com Corporation');
     $this->setModel('Generic');
     $this->setOs('3Com');
