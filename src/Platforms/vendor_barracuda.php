3fe699589f67115be6fc6813ea0b3c075df645e8
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-06-16 09:51:52 -0300

    Update

--- src/Platforms/vendor_barracuda.php
+++ src/Platforms/vendor_barracuda.php
@@ -1,11 +1,12 @@
 <?php
 
 if (stristr($sysDescr, 'Barracuda Load Balancer') || stristr($sysDescr, 'Barracuda Load Balancer ADC')) {
-    $os = 'Barracuda Load Balancer';
+    $this->setVendor('Barracuda');
+    $this->setModel('Generic');
+    $this->setOs('Barracuda Load Balancer');
 } else if (stristr($sysDescr, 'Barracuda Spam Firewall')) {
-    $os = 'Barracuda Spam Firewall';
+    $this->setVendor('Barracuda');
+    $this->setModel('Generic');
+    $this->setOs('Barracuda Spam Firewall');
 }
 
-$this->setVendor('Barracuda');
-$this->setModel('Generic');
-$this->setOs($os);
