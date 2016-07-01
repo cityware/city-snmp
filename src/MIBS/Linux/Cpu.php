18dd0e5d394ba8616f3ae6008d8b1b5539405035
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-07-01 10:42:13 -0300

    Update

--- src/MIBS/Linux/Cpu.php
+++ src/MIBS/Linux/Cpu.php
@@ -6,28 +6,24 @@ namespace Cityware\Snmp\MIBS\Linux;
  * A class for performing SNMP V2 queries on generic devices
  */
 class Cpu extends \Cityware\Snmp\MIB {
-    
+
     const OID_CPU = '.1.3.6.1.4.1.2021.11';
     const OID_LOAD = '.1.3.6.1.4.1.2021.10.1.3';
     const OID_LOAD_INT = '.1.3.6.1.4.1.2021.10.1.5';
-
     const OID_1_MINUTE_LOAD = '.1.3.6.1.4.1.2021.10.1.3.1';
     const OID_5_MINUTE_LOAD = '.1.3.6.1.4.1.2021.10.1.3.2';
     const OID_15_MINUTE_LOAD = '.1.3.6.1.4.1.2021.10.1.3.3';
-        
-    const OID_UCD_SNMPD_LALOAD_INT_1  = '.1.3.6.1.4.1.2021.10.1.5.1';
+    const OID_UCD_SNMPD_LALOAD_INT_1 = '.1.3.6.1.4.1.2021.10.1.5.1';
     const OID_UCD_SNMPD_LALOAD_INT_5 = '.1.3.6.1.4.1.2021.10.1.5.2';
     const OID_UCD_SNMPD_LALOAD_INT_15 = '.1.3.6.1.4.1.2021.10.1.5.3';
-    
     const OID_PERCENTAGE_OF_USER_CPU_TIME = '.1.3.6.1.4.1.2021.11.9.0';
     const OID_PERCENTAGES_OF_SYSTEM_CPU_TIME = '.1.3.6.1.4.1.2021.11.10.0';
     const OID_PERCENTAGES_OF_IDLE_CPU_TIME = '.1.3.6.1.4.1.2021.11.10.0';
-    
     const OID_RAW_USER_CPU_TIME = '.1.3.6.1.4.1.2021.11.50.0';
     const OID_RAW_NICE_CPU_TIME = '.1.3.6.1.4.1.2021.11.51.0';
     const OID_RAW_SYSTEM_CPU_TIME = '.1.3.6.1.4.1.2021.11.52.0';
     const OID_RAW_IDLE_CPU_TIME = '.1.3.6.1.4.1.2021.11.53.0';
-    
+
     /**
      * Returns Float Number witch Load CPU in One Minute
      * @return float
@@ -35,7 +31,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadOneMinute() {
         return (float) $this->getSNMP()->get(self::OID_1_MINUTE_LOAD);
     }
-    
+
     /**
      * Returns Float Number witch Load CPU in Five Minutes
      * @return float
@@ -43,7 +39,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadFiveMinutes() {
         return (float) $this->getSNMP()->get(self::OID_5_MINUTE_LOAD);
     }
-    
+
     /**
      * Returns Float Number witch Load CPU in Fifteen Minutes
      * @return float
@@ -51,7 +47,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadFifteenMinutes() {
         return (float) $this->getSNMP()->get(self::OID_15_MINUTE_LOAD);
     }
-    
+
     /**
      * Returns Integer Number witch Load CPU in One Minute
      * @return Integer
@@ -59,7 +55,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadIntOneMinute() {
         return $this->getSNMP()->get(self::OID_UCD_SNMPD_LALOAD_INT_1);
     }
-    
+
     /**
      * Returns Integer Number witch Load CPU in Five Minutes
      * @return Integer
@@ -67,7 +63,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadIntFiveMinutes() {
         return $this->getSNMP()->get(self::OID_UCD_SNMPD_LALOAD_INT_5);
     }
-    
+
     /**
      * Returns Integer Number witch Load CPU in Fifteen Minutes
      * @return Integer
@@ -75,7 +71,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function loadIntFifteenMinutes() {
         return $this->getSNMP()->get(self::OID_UCD_SNMPD_LALOAD_INT_15);
     }
-        
+
     /**
      * Returns Percentage of User CPU Time
      * @return float
@@ -83,7 +79,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function percentageOfUserCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_PERCENTAGE_OF_USER_CPU_TIME);
     }
-    
+
     /**
      * Returns Percentage of System CPU Time
      * @return float
@@ -91,7 +87,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function percentageOfSystemCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_PERCENTAGES_OF_SYSTEM_CPU_TIME);
     }
-    
+
     /**
      * Returns Percentage of Idle CPU Time
      * @return float
@@ -99,7 +95,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function percentageOfIdleCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_PERCENTAGES_OF_IDLE_CPU_TIME);
     }
-    
+
     /**
      * Returns Raw User CPU Time
      * @return float
@@ -107,7 +103,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function rawUserCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_RAW_USER_CPU_TIME);
     }
-    
+
     /**
      * Returns Raw Nice CPU Time
      * @return float
@@ -115,7 +111,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function rawNiceCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_RAW_NICE_CPU_TIME);
     }
-    
+
     /**
      * Returns Raw System CPU Time
      * @return float
@@ -123,7 +119,7 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function rawSystemCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_RAW_SYSTEM_CPU_TIME);
     }
-    
+
     /**
      * Returns Raw Idle CPU Time
      * @return float
@@ -131,4 +127,30 @@ class Cpu extends \Cityware\Snmp\MIB {
     public function rawIdleCpuTime() {
         return (float) $this->getSNMP()->get(self::OID_RAW_IDLE_CPU_TIME);
     }
+
+    /**
+     * Return calculation Threshoud alert CPU
+     * @param integer $numSlots
+     * @param integer $numCores
+     * @param string $ht
+     * @return array
+     */
+    public function threshoudCalculation($numSlots = 1, $numCores = 1, $ht = 'N') {
+        $totalCpu = ($ht == 'S') ? (($numSlots * $numCores) * 2) : ($numSlots * $numCores);
+
+        $aReturn = Array();
+        $aReturn['warning'] = Array();
+        $aReturn['critical'] = Array();
+
+        $aReturn['warning']['load1min'] = round(($totalCpu * 0.7), 2);
+        $aReturn['warning']['load5min'] = round(($totalCpu * 0.65), 2);
+        $aReturn['warning']['load15min'] = round(($totalCpu * 0.6), 2);
+
+        $aReturn['critical']['load1min'] = round(($totalCpu * 1.0), 2);
+        $aReturn['critical']['load5min'] = round(($totalCpu * 0.9), 2);
+        $aReturn['critical']['load15min'] = round(($totalCpu * 0.8), 2);
+
+        return $aReturn;
+    }
+
 }
