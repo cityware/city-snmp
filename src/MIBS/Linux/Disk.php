9349341df240b358d57ee37eef1b23208917d807
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-07-04 09:53:16 -0300

    Update

--- src/MIBS/Linux/Disk.php
+++ src/MIBS/Linux/Disk.php
@@ -48,7 +48,7 @@ class Disk extends \Cityware\Snmp\MIB {
         $aReturn['used_size'] = $aDisk[self::OID_DISK_USED];
         
         $aReturn['used_percent'] = $aDisk[self::OID_DISK_USED_PERCENT];
-        $aReturn['free_percent'] = 100 - $aReturn['used_percent'];
+        $aReturn['free_percent'] = ($aReturn['used_percent'] > 0) ? (100 - $aReturn['used_percent']): 100;
         $aReturn['node_percent'] = $aDisk[self::OID_DISK_NODE_PERCENT];
         $aReturn['total_low'] = $aDisk[self::OID_DISK_TOTAL_LOW];
         $aReturn['total_high'] = $aDisk[self::OID_DISK_TOTAL_HIGH];
